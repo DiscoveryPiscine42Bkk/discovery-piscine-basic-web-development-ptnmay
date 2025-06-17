b43fa1870a9d8b6c9dfd1150a4c9a17808d5c13c
MAVIS <105956506+ptnmay@users.noreply.github.com>
2025-06-17 07:28:40 +0000

    done c04

---
+++ cell00/ex04/argv.sh
@@ -0,0 +1,17 @@
+# ส่วนของเนื้อหาของไฟล์ argv.sh
+if [ $# -eq 0 ]
+then
+	echo "No arguments supplied";
+elif [ $# -eq 1 ]
+then
+	echo $1
+elif [ $# -eq 2 ] 
+then
+	echo $1
+	echo $2
+elif [ $# -eq 3 ]
+then
+	echo $1
+	echo $2
+	echo $3
+fi
---
+++ cell00/ex05/build.sh
@@ -0,0 +1,10 @@
+# ส่วนของเนื้อหาของไฟล์ build.sh
+if [ $# -eq 0 ];
+then
+	echo "No arguments supplied";
+else
+	for i in "$@";
+	do
+		touch "ex$i";
+	done
+fi
